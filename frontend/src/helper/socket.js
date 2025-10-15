16e013beaa7005ed4a1f65d298a309aa57aa40a7
Ritesh Sharma <riteshrsharma71@gmail.com>
2025-10-15 13:20:35 +0530

    Fix: updated for production

--- frontend/src/helper/socket.js
+++ frontend/src/helper/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket", "polling"], // ensures both WebSocket and polling work
